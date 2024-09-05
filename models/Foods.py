3a6640e7b81f237247392beab073e73a77ccd0c0
runfast1335 <pourkhadjeh2@gmail.com>
2024-09-05 14:15:28 +0330

    ijade mahdoodiyate name ghaza ke kamtar az 3 harf nabashad

--- models/Foods.py
+++ models/Foods.py
@@ -1,10 +1,19 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
+
 
 class Foods(models.Model):
     _name = "food_reservation.foods"
 
+
     name = fields.Char(string="نام غذا", required=True)
     food_type = fields.Selection([("poloyi", "پلویی"), ("khorack", "خوراک")], string="نوع غذا", required=True)
+
+    @api.constrains('name')
+    def _check_name(self):
+        for rec in self:
+            if len(rec.name) < 3:
+                raise ValidationError("نام نباید کمتر از 3 حرف داشته باشد")
 
 
 
